bc9e4ecd82d9abcd610201cafad61c10c700664e
Vallirie <turbanos.136564120358@depedqc.ph>
2025-11-10 23:03:13 +0800

    Add files via upload

---
+++ BASIC CONCEPTS IN C++/Untitled1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	double celcius, fahrenheit;
+	fahrenheit= 70;
+	
+	celcius = (fahrenheit-32)*5/9;
+	cout<<"celcius: "<<celcius;
+	
+	return 0;
+}
